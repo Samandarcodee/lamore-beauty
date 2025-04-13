3291082e0a222bc75a31d97794483b6a6ecb882f
Samandar Kadirov <50655131+Samandarcodee@users.noreply.github.com>
2025-04-13 05:37:39 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@chakra-ui/icons'], // Ensure Vite pre-bundles the package
+  },
   build: {
     rollupOptions: {
-      external: ['@chakra-ui/icons']
-    }
-  }
-})
+      // Remove '@chakra-ui/icons' from 'external' (if present)
+    },
+  },
+});
